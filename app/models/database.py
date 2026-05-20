f6976112f661f7792505af95cb457fd05d26644e
yubrajneupane1 <yubrajneupanech@gmail.com>
2026-05-20 09:20:00 +0545

    FINAL UPDATE

--- app/models/database.py
+++ app/models/database.py
@@ -1,13 +1,33 @@
 import pymysql
-from config import *
+import config
 
 class Database:
+    def __init__(self):
+        try:
+            self.connection = pymysql.connect(
+                host=config.MYSQL_HOST,
+                user=config.MYSQL_USER,
+                password=config.MYSQL_PASSWORD,
+                database=config.MYSQL_DATABASE,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            self.cursor = self.connection.cursor()
+            print("Database Connected")
+        except Exception as e:
+            print("Database Error:", e)
 
-    @staticmethod
-    def get_connection():
-        return pymysql.connect(
-            host=MYSQL_HOST,
-            user=MYSQL_USER,
-            password=MYSQL_PASSWORD,
-            database=MYSQL_DATABASE
-        )
+    def fetch(self, query, values=None):
+        self.cursor.execute(query, values)
+        return self.cursor.fetchall()
+
+    def fetchone(self, query, values=None):
+        self.cursor.execute(query, values)
+        return self.cursor.fetchone()
+
+    def execute(self, query, values=None):
+        self.cursor.execute(query, values)
+        self.connection.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
