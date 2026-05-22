8855f930600b3c01cb514760debe4ad39229428a
yubrajneupane1 <yubrajneupanech@gmail.com>
2026-05-22 09:14:00 +0545

    Add login route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,6 +4,10 @@ class AuthRoutes:
 
     def __init__(self):
         self.bp = Blueprint("auth", __name__)
+    
+    @self.bp.route("/login")
+    def login():
+        return render_template("login.html")
 
     def register(self):
         return self.bp
