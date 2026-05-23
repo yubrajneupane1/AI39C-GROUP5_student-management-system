f7bc8fd77e7f2223421fd1c7c5365292ecc64673
yubrajneupane1 <yubrajneupanech@gmail.com>
2026-05-23 11:05:00 +0545

    added login credential validation

--- app/controller/auth.py
+++ app/controller/auth.py
@@ -7,3 +7,13 @@ class AuthController:
         self.user_model = UserModel()
     def login(self, username):
         return self.user_model.get_user_by_username(username)
+    def validate_login(self, username, password):
+        user = self.user_model.get_user_by_username(username)
+
+        if not user:
+            return False
+
+        return check_password_hash(
+        user["password"],
+        password
+    )
