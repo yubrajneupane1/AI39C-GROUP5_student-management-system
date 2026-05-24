e9117b81a08d3910b9fcccec39125b5877f41504
yubrajneupane1 <yubrajneupanech@gmail.com>
2026-05-24 10:50:00 +0545

    FINAL UPDATE

--- app/models/basemodel.py
+++ app/models/basemodel.py
@@ -1,23 +1,64 @@
-from app.models.database import Database
-from werkzeug.security import generate_password_hash
-class BaseModel:
+from abc import ABC, abstractmethod
+from .database import Database
 
-    def __init__(self):
-        self.conn = Database.get_connection()
-        self.cursor = self.conn.cursor(dictionary=True)
-    def create_user(self, username, password, role):
+class BaseModel(ABC):
+    @property
+    @abstractmethod
+    def table(self):
+        pass
 
-        hashed_password = generate_password_hash(password)
+    @classmethod
+    def find_by_id(cls, item_id):
+        instance = cls()
+        db = Database()
+        result = db.fetchone(
+            f"SELECT * FROM {instance.table} WHERE id=%s",
+            (item_id,)
+        )
+        db.close()
+        if result:
+            return cls.from_db(result)
+        return None
 
-        query = """
-        INSERT INTO users
-        (username,password,role)
-        VALUES(%s,%s,%s)
-    """
+    @classmethod
+    def find_by(cls, column, value):
+        instance = cls()
+        db = Database()
+        result = db.fetchone(
+            f"SELECT * FROM {instance.table} WHERE {column}=%s",
+            (value,)
+        )
+        db.close()
+        if result:
+            return cls.from_db(result)
+        return None
 
-        self.cursor.execute(
-        query,
-        (username, hashed_password, role)
-    )
+    @classmethod
+    def find_all(cls):
+        instance = cls()
+        db = Database()
+        result = db.fetchall(
+            f"SELECT * FROM {instance.table}"
+        )
+        db.close()
+        return result
 
-        self.conn.commit()
+    @classmethod
+    def count_all(cls):
+        instance = cls()
+        db = Database()
+        result = db.fetchone(
+            f"SELECT COUNT(*) as total FROM {instance.table}"
+        )
+        db.close()
+        return result["total"]
+
+    @classmethod
+    def delete_by_id(cls, item_id):
+        instance = cls()
+        db = Database()
+        db.execute(
+            f"DELETE FROM {instance.table} WHERE id=%s",
+            (item_id,)
+        )
+        db.close()
