82d6c346fe143134d187dd8e4f9760b76f24d48a
yubrajneupane1 <yubrajneupanech@gmail.com>
2026-05-24 10:41:00 +0545

    Implement user creation method

--- app/models/basemodel.py
+++ app/models/basemodel.py
@@ -5,3 +5,19 @@ class BaseModel:
     def __init__(self):
         self.conn = Database.get_connection()
         self.cursor = self.conn.cursor(dictionary=True)
+    def create_user(self, username, password, role):
+
+        hashed_password = generate_password_hash(password)
+
+        query = """
+        INSERT INTO users
+        (username,password,role)
+        VALUES(%s,%s,%s)
+    """
+
+        self.cursor.execute(
+        query,
+        (username, hashed_password, role)
+    )
+
+        self.conn.commit()
