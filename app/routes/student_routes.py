163ca14c0821669f462324ba8964c54743379f1e
nischlkunwr617 <nischalkunwar.617@gmail.com>
2026-05-26 03:12:00 +0545

    Add student dashboard route

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -1,7 +1,12 @@
 from flask import Blueprint
-
+from flask import render_template
 student_bp = Blueprint(
     "student",
     __name__,
     url_prefix="/student"
 )
+@student_bp.route("/dashboard")
+def dashboard():
+    return render_template(
+        "student/dashboard.html"
+    )
