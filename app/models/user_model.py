d438ea2b64df7e32141f6a6e89027e3aa744f265
yubrajneupane1 <yubrajneupanech@gmail.com>
2026-05-26 09:11:00 +0545

    Add password verification method

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,8 +1,14 @@
 from app.models.basemodel import BaseModel
-
+from werkzeug.security import check_password_hash
 class UserModel(BaseModel):
 
     def get_user_by_username(self, username):
         query = "SELECT * FROM users WHERE username=%s"
         self.cursor.execute(query, (username,))
         return self.cursor.fetchone()
+ 
+    def check_password(self, plain_password):
+       return check_password_hash(
+        self._password,
+        plain_password
+    )
