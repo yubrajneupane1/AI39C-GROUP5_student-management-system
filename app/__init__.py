3c3262f439c925c3daccbb2b6010f144bfc035bb
Reeyush <riyushawal66@gmail.com>
2026-06-03 04:19:00 +0545

    app/models/attendance_model.py

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from app.routes.auth import AuthRoutes
+from app.routes.student_routes import student_bp
+from app.routes.admin_routes import admin_bp
+from app.routes.teacher_routes import teacher_bp
+from app.routes.profile_routes import profile_bp
+from app.routes.attendance_routes import attendance_bp
+from app.routes.notification_routes import notification_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "secretkey"
+    
+    auth_routes = AuthRoutes()
+    app.register_blueprint(auth_routes.register())
+    
+    app.register_blueprint(student_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(teacher_bp)
+    app.register_blueprint(profile_bp)
+    app.register_blueprint(attendance_bp)
+    app.register_blueprint(notification_bp)
+    
+    return app
