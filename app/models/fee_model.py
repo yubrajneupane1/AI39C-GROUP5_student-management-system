241b3f00015aeb0724573f962d72be79f09d32de
mubarak1915-ops <mubarak1915@xavier.edu.np>
2026-06-06 11:08:00 +0545

    Add pending fee retrieval functionality

--- app/models/fee_model.py
+++ app/models/fee_model.py
@@ -6,3 +6,11 @@ def get_all_fees(self):
     query = "SELECT * FROM fees"
     self.cursor.execute(query)
     return self.cursor.fetchall()
+def get_pending_fees(self):
+    query = """
+    SELECT *
+    FROM fees
+    WHERE status='Pending'
+    """
+    self.cursor.execute(query)
+    return self.cursor.fetchall()
